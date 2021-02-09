d1f16d2fe5cec40d0afa09bf2e91e66cb280ba60
Nick <67390674+DDoSdbyDoor@users.noreply.github.com>
2021-02-09 13:42:54 -0500

    Mid_Sprint_1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy (gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+}
