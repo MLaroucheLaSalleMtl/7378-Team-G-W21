a4e2422fe029d1914594a1d6cb1d882553265bb9
Nick <67390674+DDoSdbyDoor@users.noreply.github.com>
2021-02-11 19:31:07 -0500

    Sprint 1 complete
    +assets
    +scripts

--- Assets/Scripts/MyCharacterController.cs
+++ Assets/Scripts/MyCharacterController.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class MyCharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float verticalVelocity;
+
+    public Collider[] attackHitBoxes;
+    public Collider[] attackHurtBoxes;
+
+
     void Start()
     {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))    
+        {
+            LaunchAttack(attackHitBoxes[0]);
+            LaunchAttack(attackHurtBoxes[0]);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            LaunchAttack(attackHitBoxes[1]);
+            LaunchAttack(attackHurtBoxes[1]);
+        }
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = 10;
+            }
+        }
+        else
+        {
+            verticalVelocity -= 14 * Time.deltaTime;
+        }
+
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * 5;
+        moveVector.y = verticalVelocity;
+
+        controller.Move(moveVector * Time.deltaTime);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LaunchAttack(Collider col)
     {
-        
+        var cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hurtbox"));
+        foreach(Collider c in cols)
+        {
+            if (c.transform.root == transform) //used to check if im hitting my self 
+            {
+                continue;
+            }
+
+            Debug.Log(c.name);
+  
+        }
     }
+
 }
+
+
+
+// how to do damage
+//float damage = 0; 
+//switch (c.name)
+//{
+//    case "Head":
+//        damage = 30;
+//        break;
+//    case "torso":
+//        damage = 10;
+//        break;
+//    default:
+//        Debug.Log("Unable to identify body part, name must match case");
+//        break;
+//}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Transform[] playerTransforms;
+    [SerializeField] private float yOffset = 2.0f;
+    [SerializeField] private float minDistance = 7.5f;
+
+    [SerializeField] private float xMin, xMax, yMin, yMax;
+    private float xMiddle;
+    private float yMiddle;
+    private float distance;
+
+    void Start()
+    {
+        GameObject[] playerList = GameObject.FindGameObjectsWithTag("Player");
+        playerTransforms = new Transform[playerList.Length];
+        for(int i = 0; i < playerList.Length; i++)
+        {
+            playerTransforms[i] = playerList[i].transform;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(playerTransforms.Length == 0)
+        {
+            return;
+        }
+
+        xMin = xMax = playerTransforms[0].position.x;
+        yMin = yMax = playerTransforms[0].position.y;
+
+        for(int i = 1; i < playerTransforms.Length; i++)
+        {
+            if(playerTransforms[i].position.x < xMin)
+            {
+                xMin = playerTransforms[i].position.x;
+            }
+
+            if (playerTransforms[i].position.x > xMax)
+            {
+                xMax = playerTransforms[i].position.x;
+            }
+
+            if (playerTransforms[i].position.x < yMin)
+            {
+                yMin = playerTransforms[i].position.y;
+            }
+
+            if (playerTransforms[i].position.x > yMax)
+            {
+                yMax = playerTransforms[i].position.y;
+            }
+        }
+
+        xMiddle = (xMin + xMax) / 2;
+        yMiddle = (yMin + yMax) / 2;
+        distance = xMax - xMin;
+
+        if(distance < minDistance)
+        {
+            distance = minDistance;
+        }
+
+        transform.position = new Vector3(xMiddle, yMiddle + yOffset, distance - 10);
+    }
+}
